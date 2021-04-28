4419c3470ade926b9f95c38ba07ba24aa1b77724
fengzie <526567244@qq.com>
2021-04-28 10:36:05 +0800

    更新verified moderators接口

--- js/models/LocalSettings.js
+++ js/models/LocalSettings.js
@@ -50,7 +50,7 @@ export default class extends Model {
       language,
       listingsGridViewType: 'grid',
       bitcoinUnit: 'BTC',
-      verifiedModsProvider: 'https://search.ob1.io/verified_moderators',
+      verifiedModsProvider: 'https://market.mobazha.com/api/verified_moderators',
       verifiedModsProviderTor: 'http://my7nrnmkscxr32zo.onion/verified_moderators',
       dontShowTorExternalLinkWarning: false,
     };

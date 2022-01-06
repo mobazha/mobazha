103e184f37980176bc5be59bdf2de9c6383089c4
fengzie <526567244@qq.com>
2022-01-06 10:38:37 +0800

    update react-native-config version

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,10 +1,8 @@
 import Reactotron from 'reactotron-react-native';
 import { reactotronRedux } from 'reactotron-redux';
 
-if(__DEV__) {
-  Reactotron
-    .configure({ host: 'localhost' }) // controls connection & communication settings
-    .use(reactotronRedux())
-    .useReactNative() // add all built-in react native plugins
-    .connect(); // let's connect!
-}
+Reactotron
+  .configure({ host: 'localhost' }) // controls connection & communication settings
+  .use(reactotronRedux())
+  .useReactNative() // add all built-in react native plugins
+  .connect(); // let's connect!

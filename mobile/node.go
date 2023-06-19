16ebc17f297ce93ca9fedb356f8b6632f388577b
fengzie <526567244@qq.com>
2023-06-19 13:21:07 +0800

    Add BNB support

--- mobile/node.go
+++ mobile/node.go
@@ -82,7 +82,7 @@ func NewNode(cfg *Config) (*Node, error) {
 	rcfg := &repo.Config{
 		IPNSQuorum:             2,
 		LogLevel:               logLevel,
-		EnabledWallets:         []string{"BTC", "BCH", "LTC"},
+		EnabledWallets:         []string{"BTC", "BCH", "LTC", "BNB"},
 		DisableNATPortMap:      true,
 		DataDir:                dataDir,
 		LogDir:                 logDir,

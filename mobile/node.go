ae22b159d74839f87922022f9b53a6880a82c76e
fengzie <526567244@qq.com>
2023-07-21 09:58:09 +0800

    Add MATIC for mobile

--- mobile/node.go
+++ mobile/node.go
@@ -82,7 +82,7 @@ func NewNode(cfg *Config) (*Node, error) {
 	rcfg := &repo.Config{
 		IPNSQuorum:             2,
 		LogLevel:               logLevel,
-		EnabledWallets:         []string{"BTC", "BCH", "LTC", "BNB", "USDT", "USDC", "MBZ"},
+		EnabledWallets:         []string{"BTC", "BCH", "LTC", "MATIC", "MATICUSDT", "MATICUSDC", "MATICMBZ"},
 		DisableNATPortMap:      true,
 		DataDir:                dataDir,
 		LogDir:                 logDir,

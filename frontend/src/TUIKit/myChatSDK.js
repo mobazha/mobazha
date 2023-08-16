7d9472f0604e7d29f4dfc3258d98d73ee9d42490
fengzie <526567244@qq.com>
2023-08-16 11:16:59 +0800

    Add MyChatSDK

---
+++ frontend/src/TUIKit/myChatSDK.js
@@ -0,0 +1,140 @@
+"use strict";
+
+class MyChatSDK {
+  on() {}
+  off() {}
+
+  sendMessage() {
+    // TODO:
+  }
+  createFaceMessage() {}
+  createImageMessage() {}
+  createVideoMessage() {}
+  createFileMessage() {}
+  createCustomMessage() {}
+  createLocationMessage() {}
+  createForwardMessage() {}
+  sendMessageReadReceipt() {}
+  getMessageReadReceiptList() {}
+
+  getMessageList() {
+    // TODO:
+  }
+  createTextMessage() {
+    // TODO:
+  }
+  createTextAtMessage() {}
+  createMergerMessage() {}
+  revokeMessage() {}
+  resendMessage() {}
+  deleteMessage() {}
+  modifyMessage() {}
+  findMessage() {}
+
+  getUserProfile() {
+    // TODO:
+  }
+  getMyProfile() {
+    // TODO:
+  }
+  updateMyProfile() {
+    // TODO:
+  }
+
+  getFriendList() {}
+  checkFriend() {}
+  
+  setMessageRead() {}
+
+  getUserStatus() {}
+  subscribeUserStatus() {}
+  unsubscribeUserStatus() {}
+
+  getConversationList() {
+    // TODO:
+  }
+  getConversationProfile() {
+    // TODO:
+  }
+  deleteConversation() {}
+  pinConversation() {}
+  setMessageRemindType() {}
+
+  createGroup() {}
+  getGroupMessageReadMemberList() {}
+  getGroupProfile() {}
+  getGroupMemberProfile() {}
+  handleGroupApplication() {}
+  getGroupList() {}
+  dismissGroup() {}
+  updateGroupProfile() {}
+  joinGroup() {}
+  quitGroup() {}
+  searchGroupByID() {}
+  getGroupOnlineMemberCount() {}
+  changeGroupOwner() {}
+  initGroupAttributes() {}
+  setGroupAttributes() {}
+  deleteGroupAttributes() {}
+  getGroupAttributes() {}
+  getGroupMemberList() {}
+  addGroupMember() {}
+  deleteGroupMember() {}
+  setGroupMemberMuteTime() {}
+  setGroupMemberRole() {}
+  setGroupMemberNameCard() {}
+}
+
+class ProfileHandler {
+  constructor(e) {
+    this._n = "ProfileHandler";
+    this.TAG = "profile";
+  }
+
+  setExpirationTime(e) {
+    this.expirationTime = e;
+  }
+
+  getUserProfile(userIDList) {}
+
+  getMyProfile() {}
+}
+
+class C2CModule {
+  setMessageRead(options) {}
+
+  deleteConversation(options) {}
+
+  pinConversation() {}
+
+  setMessageRemindType() {}
+
+  getConversationProfile() {}
+
+  getConversationList() {}
+}
+
+class GroupHandler {}
+
+/* Events
+this.TUICore.TIM.EVENT.MESSAGE_RECEIVED
+this.TUICore.TIM.EVENT.MESSAGE_MODIFIED
+this.TUICore.TIM.EVENT.MESSAGE_REVOKED
+this.TUICore.TIM.EVENT.MESSAGE_READ_BY_PEER
+
+this.TUICore.TIM.EVENT.GROUP_LIST_UPDATED
+
+this.TUICore.TIM.EVENT.MESSAGE_READ_RECEIPT_RECEIVED
+
+this.TUICore.TIM.EVENT.GROUP_ATTRIBUTES_UPDATED
+this.TUICore.TIM.EVENT.CONVERSATION_LIST_UPDATED
+this.TUICore.TIM.EVENT.FRIEND_LIST_UPDATED
+this.TUICore.TIM.EVENT.USER_STATUS_UPDATED
+this.TUICore.TIM.EVENT.NET_STATE_CHANGE
+this.TUICore.TIM.EVENT.PROFILE_UPDATED
+*/
+
+/* TODO
+this.TUICore.TIM.EVENT.MESSAGE_RECEIVED
+this.TUICore.TIM.EVENT.CONVERSATION_LIST_UPDATED
+*/

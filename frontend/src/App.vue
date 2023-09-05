b240c1563012c4060420b1c291218a253b24dbd0
fengzie <526567244@qq.com>
2023-09-05 14:10:37 +0800

    Add FormError

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <router-view/>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {},
+  data() {
+    return {};
+  },
+  watch: {},
+  methods: {}
+}
+</script>
+<style lang="less"></style>
---
+++ frontend/src/components/global/FormError.vue
@@ -0,0 +1,15 @@
+<template>
+  <ul class="unstyled errorList js-errorList <%= ob.class || ''%>" :id="id">
+    <li v-for="(err, k) in errors" :key="k">
+      <i class="ion-alert-circled"></i>{{ err }}
+    </li>
+  </ul>
+</template>
+  
+<script setup>
+const props = defineProps({
+  errors: [],
+  id: String,
+})
+</script>
+<style lang="scss" scoped></style>

80ed05d3185df5637bb1fc942e054f7c354d1584
fengzie <526567244@qq.com>
2023-09-11 14:26:46 +0800

    Add summaryTab sub pages

---
+++ frontend/src/views/modals/orderDetail/summaryTab/Accepted.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="acceptedEvent rowLg">
+    <h2 class="tx4 margRTn">{{ ob.polyT('orderDetail.summaryTab.accepted.heading') }}</h2>
+    <div v-if="ob.timestamp">
+      <span class="clrT2 tx5b">{{ moment(ob.timestamp).format('lll') }}</span>
+    </div>
+    <div class="border clrBr padMd">
+      <div class="flexVCent gutterH clrT">
+        <div class="avatarCol disc clrBr2 clrSh1 flexNoShrink" :style="ob.getAvatarBgImage(ob.avatarHashes)"></div>
+        <div class="flexExpand tx5">
+          <div class="rowTn txB">{{ ob.polyT('orderDetail.summaryTab.accepted.orderAccepted') }}</div>
+          <div>{{ infoText }}</div>
+        </div>
+        <div class="col">
+          <div class="flexVCent gutterHLg">
+            <div v-if="ob.showRefundButton">
+              <div v-if="refundOrderInProgress">
+                <span class="posR">
+                  <!-- // including invisible refund link to properly space the spinner -->
+                  <a class="txU tx6 invisible">{{ ob.polyT('orderDetail.summaryTab.accepted.refundBtn') }}</a>
+                  <SpinnerSVG className="spinnerSm center" />
+                </span>
+              </div>
+
+              <div v-else>
+                <div class="posR">
+                  <a class="txU tx6" :disabled="refundConfirmOn || fulfillInProgress" @click="onClickRefundOrder">{{
+                    ob.polyT('orderDetail.summaryTab.accepted.refundBtn') }}</a>
+                  <div v-if="refundConfirmOn">
+                    <div class="confirmBox refundConfirm tx5 arrowBoxTop clrBr clrP clrT"
+                      @click="onClickRefundConfirmBox">
+                      <div class="tx3 txB rowSm">{{ ob.polyT('orderDetail.summaryTab.accepted.refundConfirm.title') }}
+                      </div>
+                      <p>
+                        {{
+                          ob.polyT('orderDetail.summaryTab.accepted.refundConfirm.body',
+                            {
+                              cur: ob.polyT(`cryptoCurrencies.${ob.paymentCoin}`, { _: ob.paymentCoin, }),
+                            }
+                          )
+                        }}
+                      </p>
+                      <hr class="clrBr row" />
+                      <div class="flexHRight flexVCent gutterHLg buttonBar">
+                        <a @click="onClickRefundConfirmCancel">{{ ob.polyT('orderDetail.summaryTab.accepted.refundConfirm.btnCancel') }}</a>
+                        <a class="btn clrBAttGrad clrBrDec1 clrTOnEmph" @click="onClickRefundConfirmed">{{ ob.polyT('orderDetail.summaryTab.accepted.refundConfirm.btnConfirm') }}</a>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div v-if="showFulfillButton">
+              <ProcessingButton
+                :className="`btn clrBAttGrad clrBrDec1 clrTOnEmph tx5b js-fulfillOrder ${fulfillInProgress ? 'processing' : ''}`"
+                :disabled="refundOrderInProgress" :btnText="ob.polyT('orderDetail.summaryTab.accepted.fulfillBtn')"
+                @click="onClickFulfillOrder" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import $ from 'jquery';
+import moment from 'moment';
+import {
+  fulfillingOrder,
+  refundingOrder,
+  refundOrder,
+  events as orderEvents,
+} from '../../../../../backbone/utils/order';
+import { recordEvent } from '../../../../../backbone/utils/metrics';
+
+
+export default {
+  mixins: [],
+  props: {
+    cart: Object,
+  },
+  data () {
+    return {
+      infoText: '',
+      showRefundButton: false,
+      showFulfillButton: false,
+      avatarHashes: {},
+      refundConfirmOn: false,
+      paymentCoin: undefined,
+
+      fulfillInProgress: false,
+      refundOrderInProgress: false,
+    };
+  },
+  created () {
+    this.loadData(this.$props);
+  },
+  mounted () {
+  },
+  computed: {
+  },
+  methods: {
+    moment,
+
+    loadData (options = {}) {
+      if (!options.orderID) {
+        throw new Error('Please provide the order id.');
+      }
+
+      this.orderID = options.orderID;
+
+      this.fulfillInProgress = fulfillingOrder(this.orderID);
+      this.refundOrderInProgress = refundingOrder(this.orderID);
+
+      this.listenTo(orderEvents, 'fulfillingOrder', e => {
+        if (e.id === this.orderID) {
+          this.fulfillInProgress = true;
+        }
+      });
+
+      this.listenTo(orderEvents, 'fulfillOrderComplete fulfillOrderFail', e => {
+        if (e.id === this.orderID) {
+          this.fulfillInProgress = false;
+        }
+      });
+
+      this.listenTo(orderEvents, 'refundingOrder', e => {
+        if (e.id === this.orderID) {
+          this.refundOrderInProgress = true;
+        }
+      });
+
+      this.listenTo(orderEvents, 'refundOrderComplete refundOrderFail', e => {
+        if (e.id === this.orderID) {
+          this.refundOrderInProgress = false;
+        }
+      });
+
+      this.boundOnDocClick = this.onDocumentClick.bind(this);
+      $(document).on('click', this.boundOnDocClick);
+    },
+
+    onClickRefundOrder () {
+      recordEvent('OrderDetails_Refund');
+      this.refundConfirmOn = true;
+      return false;
+    },
+
+    onClickRefundConfirmBox () {
+      // ensure event doesn't bubble so onDocumentClick doesn't
+      // close the confirmBox.
+      return false;
+    },
+
+    onClickRefundConfirmCancel () {
+      recordEvent('OrderDetails_RefundCancel');
+      this.refundConfirmOn = false;
+    },
+
+    onDocumentClick () {
+      this.refundConfirmOn = false;
+    },
+
+    onClickRefundConfirmed () {
+      recordEvent('OrderDetails_RefundConfirm');
+      this.refundConfirmOn = false;
+      refundOrder(this.orderID);
+    },
+
+    onClickFulfillOrder () {
+      recordEvent('OrderDetails_Fulfill');
+      this.$emit('clickFulfillOrder');
+    },
+
+    remove () {
+      $(document).off('click', this.boundOnDocClick);
+    },
+  }
+}
+</script>
+<style lang="scss" scoped></style>
---
+++ frontend/src/views/modals/orderDetail/summaryTab/CompleteOrderForm.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="completeOrderForm rowLg">
+    <h2 class="tx4 margRTn">{{ ob.polyT('orderDetail.summaryTab.completeOrderForm.heading') }}</h2>
+    <div class="border clrBr padMd">
+      <div class="flex gutterHLg">
+        <div class="col9">
+          <div class="flexVBase rowSm">
+            <label class="txB tx5 required flexNoShrink" for="completeOrderReview">{{ ob.polyT('orderDetail.summaryTab.completeOrderForm.reviewLabel') }}</label>
+            <div class="flexHRight">
+              <span class="clrT2 tx6">{{ ob.polyT('orderDetail.summaryTab.completeOrderForm.maxReviewChars', { max: constraints.maxReviewCharacters}) }}</span>
+            </div>
+          </div>
+          <FormError v-if="errors.review" :errors="errors.review" />
+          <textarea rows="8" name="review" class="clrBr clrP clrSh2 rowMd" id="completeOrderReview"
+            placeholder="Write your review here…" :maxlength="constraints.maxReviewCharacters" v-model="rating.review" />
+          <div class="flexVCent gutterH">
+            <ProcessingButton
+              :className="`btn clrBAttGrad clrBrDec1 clrTOnEmph js-completeOrder ${isCompleting ? 'processing' : ''}`"
+              :btnText="ob.polyT('orderDetail.summaryTab.completeOrderForm.btnCompleteOrder')"
+              @click="onClickCompleteOrder" />
+            <div class="gutterHSm">
+              <FormError v-if="errors.anonymous" :errors="errors.anonymous" />
+              <input type="checkbox" name="anonymous" id="completeOrderAnon" class="centerLabel" data-var-type="boolean"
+                :checked="!rating.anonymous">
+              <label for="completeOrderAnon" class="clrT2 tx5b">{{ ob.polyT('orderDetail.summaryTab.completeOrderForm.anonCheckLabel') }}</label>
+            </div>
+          </div>
+        </div>
+        <div class="col3 ratingsCol">
+          <div class="row">
+            <div class="txB tx5">{{ ob.polyT('ratingLabels.overall') }}</div>
+            <FormError v-if="errors.overall" :errors="errors.overall" />
+            <div class="ratingsContainer" data-rating-type="overall"></div>
+          </div>
+          <div class="row">
+            <div class="txB tx5">{{ ob.polyT('ratingLabels.quality') }}</div>
+            <FormError v-if="errors.quality" :errors="errors.quality" />
+            <div class="ratingsContainer" data-rating-type="quality"></div>
+          </div>
+          <div class="row">
+            <div class="txB tx5">{{ ob.polyT('ratingLabels.asAdvertised') }}</div>
+            <FormError v-if="errors.description" :errors="errors.description" />
+            <div class="ratingsContainer" data-rating-type="description"></div>
+          </div>
+          <div class="row">
+            <div class="txB tx5">{{ ob.polyT('ratingLabels.delivery') }}</div>
+            <FormError v-if="errors.deliverySpeed" :errors="errors.deliverySpeed" />
+            <div class="ratingsContainer" data-rating-type="deliverySpeed"></div>
+          </div>
+          <div class="row">
+            <div class="txB tx5">{{ ob.polyT('ratingLabels.service') }}</div>
+            <FormError v-if="errors.customerService" :errors="errors.customerService" />
+            <div class="ratingsContainer" data-rating-type="customerService"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import $ from 'jquery';
+import {
+  completeOrder,
+  completingOrder,
+  events as orderEvents,
+} from '../../../../../backbone/utils/order';
+import { recordEvent } from '../../../../../backbone/utils/metrics';
+import Rating from '../../../../../backbone/models/order/orderCompletion/Rating';
+import RatingsStrip from '../../../../../backbone/views/RatingsStrip';
+
+
+export default {
+  mixins: [],
+  props: {
+    cart: Object,
+  },
+  data () {
+    return {
+      ob: {},
+    };
+  },
+  created () {
+    this.loadData(this.$props);
+  },
+  mounted () {
+    this.render();
+  },
+  computed: {
+    errors () {
+      return this.rating.validationError || {};
+    },
+    constraints () {
+      return this.rating.constraints || {};
+    }
+  },
+  methods: {
+    loadData (options = {}) {
+      super(options);
+
+      if (!options.model) {
+        throw new Error('Please provide an OrderCompletion model.');
+      }
+
+      if (!options.slug) {
+        throw new Error('Please provide the listing slug.');
+      }
+
+      this.ratingStrips = {};
+      this.slug = options.slug;
+
+      const ratings = this.model.get('ratings');
+
+      if (ratings.length) {
+        this.rating = ratings.at(0);
+      } else {
+        this.rating = new Rating();
+        ratings.push(this.rating);
+      }
+
+      this.listenTo(orderEvents, 'completingOrder', () => {
+        this.getCachedEl('.js-completeOrder').addClass('processing');
+      });
+
+      this.listenTo(orderEvents, 'completeOrderComplete completeOrderFail', () => {
+        this.getCachedEl('.js-completeOrder').removeClass('processing');
+      });
+    },
+
+    onClickCompleteOrder () {
+      const formData = this.getFormData();
+
+      const data = {
+        ...formData,
+        anonymous: !formData.anonymous,
+        // If a rating is not set, the RatingStrip view will return 0. We'll
+        // send undefined in that case since it gives us the error message we
+        // prefer.
+        overall: this.ratingStrips.overall.rating || undefined,
+        quality: this.ratingStrips.quality.rating || undefined,
+        description: this.ratingStrips.description.rating || undefined,
+        deliverySpeed: this.ratingStrips.deliverySpeed.rating || undefined,
+        customerService: this.ratingStrips.customerService.rating || undefined,
+        slug: this.slug,
+      };
+
+      this.rating.set(data);
+      this.rating.set(data, { validate: true });
+
+      if (!this.rating.validationError) {
+        completeOrder(this.model.id, this.model.toJSON());
+        recordEvent('OrderDetails_CompleteOrder');
+      }
+
+      this.render();
+      const $firstErr = $('.errorList:first');
+      if ($firstErr.length) $firstErr[0].scrollIntoViewIfNeeded();
+    },
+
+    render () {
+      this.isCompleting = !!completingOrder(this.model.id);
+
+      $('.ratingsContainer').each((index, element) => {
+        const $el = $(element);
+        const type = $el.data('ratingType');
+
+        if (!type) {
+          throw new Error('Unable to render a ratings strips because it\'s container does not ' +
+            'specify a type.');
+        }
+
+        if (this.ratingStrips[type]) this.ratingStrips[type].remove();
+        this.ratingStrips[type] = this.createChild(RatingsStrip, {
+          initialState: {
+            curRating: this.rating.get(type) || 0,
+            clickable: true,
+          },
+        });
+
+        $el.append(this.ratingStrips[type].render().el);
+      });
+
+      return this;
+    }
+
+  }
+}
+</script>
+<style lang="scss" scoped></style>

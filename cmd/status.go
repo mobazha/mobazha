03f0cf16671b76a198652dc4e18ff421c08ad025
fengzie <526567244@qq.com>
2023-12-27 21:19:21 +0800

    Add Status command

---
+++ cmd/status.go
@@ -0,0 +1,60 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/cpacia/openbazaar3.0/repo"
+	"github.com/ipfs/kubo/repo/fsrepo"
+)
+
+type Status struct {
+	DataDir string `short:"d" long:"datadir" description:"specify the data directory to be used"`
+	Testnet bool   `short:"t" long:"testnet" description:"use the test network"`
+}
+
+func (x *Status) Execute(args []string) error {
+	// Set repo path
+	if x.DataDir == "" {
+		x.DataDir = repo.DefaultHomeDir
+		if x.Testnet {
+			x.DataDir = repo.DefaultHomeDir + "-testnet"
+		}
+	}
+
+	torAvailable := false
+	isDBEncrypted := false
+
+	if fsrepo.IsInitialized(x.DataDir) {
+
+		if isDBEncrypted {
+			if !torAvailable {
+				fmt.Println("Initialized - Encrypted")
+				os.Exit(30)
+			} else {
+				fmt.Println("Initialized - Encrypted")
+				fmt.Println("Tor Available")
+				os.Exit(31)
+			}
+		} else {
+			if !torAvailable {
+				fmt.Println("Initialized - Not Encrypted")
+				os.Exit(20)
+			} else {
+				fmt.Println("Initialized - Not Encrypted")
+				fmt.Println("Tor Available")
+				os.Exit(21)
+			}
+		}
+	} else {
+		if !torAvailable {
+			fmt.Println("Not initialized")
+			os.Exit(10)
+		} else {
+			fmt.Println("Not initialized")
+			fmt.Println("Tor Available")
+			os.Exit(11)
+		}
+	}
+	return nil
+}

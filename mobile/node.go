f7edad2200b92d77baf6eea2bd5dbc19d31f81cd
fengzie <526567244@qq.com>
2024-04-16 10:15:35 +0800

    Switch back to Polygon

--- mobile/node.go
+++ mobile/node.go
@@ -82,7 +82,7 @@ func NewNode(cfg *Config) (*Node, error) {
 	rcfg := &repo.Config{
 		IPNSQuorum:             2,
 		LogLevel:               logLevel,
-		EnabledWallets:         []string{"BCH", "CFX", "CFXUSDT", "CFXUSDC", "CFXMBZ"},
+		EnabledWallets:         []string{"BCH", "MATIC", "MATICUSDT", "MATICUSDC", "MATICMBZ"},
 		DisableNATPortMap:      true,
 		DataDir:                dataDir,
 		LogDir:                 logDir,

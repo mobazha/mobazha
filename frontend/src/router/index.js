67ff91c682f8ef7a2d60b2169cea1dd78f2e9d61
fengzie <526567244@qq.com>
2024-08-07 08:28:53 +0800

    Update router history mode

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import routerMap from './routerMap'
 
 const Router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes: routerMap,
 })
 

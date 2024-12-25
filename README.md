3afcc0dc97f62d45fff259065b8fc2d6d6b6634d
fengzie <526567244@qq.com>
2024-12-25 11:01:25 +0800

    Add build script

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is the reference client for the Mobazha network. It is an interface for you
 
 For full installable versions of the Mobazha app, with the server and client bundled together, go to [the Mobazha download page.](https://www.mobazha.org/download/)
 
-[![Build Status](https://travis-ci.org/Mobazha/mobazha-desktop.svg?branch=master)](https://travis-ci.org/Mobazha/mobazha-desktop)
+[![Build Status](https://github.com/mobazha/mobazha-desktop/actions/workflows/build.yml/badge.svg)](https://github.com/mobazha/mobazha-desktop/actions)
 
 ## Getting Started
 
@@ -24,12 +24,39 @@ Download the Mobazha node for your current OS version from latest [Mobazha node 
 3. In the root subfolder, enter `npm install`.
 4. In the root folder, run `npm run dev`
 
+## Building
+
+To build the application for different platforms, use the following commands:
+
+- For Windows: `npm run build-w`
+- For macOS: `npm run build-m`
+- For Linux: `npm run build-l`
 
 ## Built With
 
 * [Electron](https://electron.atom.io/)
 * [Vue](https://vuejs.org/)
 * [Backbone](http://backbonejs.org/)
+
+## Demo
+
+### Bitcoin
+[![Bitcoin](https://img.youtube.com/vi/WSvLQRjqdX4/0.jpg)](https://www.youtube.com/watch?v=WSvLQRjqdX4)
+
+### Bitcoin Cash
+[![Bitcoin Cash](https://img.youtube.com/vi/Abo7YJYJgrE/0.jpg)](https://www.youtube.com/watch?v=Abo7YJYJgrE)
+
+### ZCash
+[![ZCash](https://img.youtube.com/vi/dWXfLLdqUEI/0.jpg)](https://www.youtube.com/watch?v=dWXfLLdqUEI)
+
+### Litecoin
+[![Litecoin](https://img.youtube.com/vi/9do7zQ8ey90/0.jpg)](https://www.youtube.com/watch?v=9do7zQ8ey90)
+
+### Web and Moderation
+[![Web and Moderation](https://img.youtube.com/vi/_PUywMOV8nk/0.jpg)](https://www.youtube.com/watch?v=_PUywMOV8nk)
+
+### Remote Store Setup
+[![Remote Store Setup](https://img.youtube.com/vi/i84R35SCkUA/0.jpg)](https://www.youtube.com/watch?v=i84R35SCkUA)
 
 ## Contributing
 
@@ -40,3 +67,6 @@ You can also look for issues with the [bug label](https://github.com/mobazha/mob
 Contributions are expected to match the coding style already present in this repo, and must pass es-lint with no errors.
 
 Contributions that make visual changes are also expected to match the repo's current style.
+
+## License
+This project is licensed under the GPLv3 License. You can view [LICENSE.MD](https://github.com/mobazha/mobazha/blob/main/LICENSE) for more details.
--- build.sh
+++ build.sh
@@ -3,153 +3,76 @@
 ## Version 3.0.0
 ##
 ## Usage
-## ./build.sh
+## ./build.sh <platform>
 ##
 ## OS supported:
 ## win32 win64 linux32 linux64 linuxarm osx
 ##
 
-
 ELECTRONVER=22.0.0
-
-BINARY="${1}"
-TRAVIS_OS_NAME="osx"
-# if [ -z "${2}" ]; then
-# SERVERTAG='latest'
-# else
-# SERVERTAG=tags/${2}
-# fi
-# echo "Building with mobazha/$SERVERTAG"
 
 # Get Version
 PACKAGE_VERSION=$(node -p 'require("./package").version')
 echo "Mobazha Version: $PACKAGE_VERSION"
 
-# Create temp build dirs
-# mkdir dist/
-# rm -rf dist/*
-# mkdir MOBAZHA_TEMP/
-# rm -rf MOBAZHA_TEMP/*
-
 echo 'Preparing to build installers...'
 
-echo 'Installing npm packages...'
-#npm i -g npm@5.2
-# npm install electron-packager -g --silent
-# npm install npm-run-all -g --silent
-# npm install grunt-cli -g --silent
-# npm install grunt --save-dev --silent
-# npm install grunt-electron-installer --save-dev --silent
-# npm install --silent
+echo 'Downloading Mobazha node...'
 
-# rvm reinstall ruby
+mkdir -p build/extraResources/mobazha
+
+if [ "$1" == "linux" ]; then
+  if [ ! -f build/extraResources/mobazha/mobazhad ]; then
+    curl -L -o build/extraResources/mobazha/mobazhad https://github.com/mobazha/mobazha-go/releases/latest/download/mobazhad-linux-amd64
+    chmod +x build/extraResources/mobazha/mobazhad
+  else
+    echo "Mobazha node already exists for Linux"
+  fi
+elif [ "$1" == "osx" ]; then
+  if [ ! -f build/extraResources/mobazha/mobazhad ]; then
+    curl -L -o build/extraResources/mobazha/mobazhad https://github.com/mobazha/mobazha-go/releases/latest/download/mobazhad-darwin-amd64
+    chmod +x build/extraResources/mobazha/mobazhad
+  else
+    echo "Mobazha node already exists for macOS"
+  fi
+elif [ "$1" == "win" ]; then
+  if [ ! -f build/extraResources/mobazha/mobazha.exe ]; then
+    curl -L -o build/extraResources/mobazha/mobazha.exe https://github.com/mobazha/mobazha-go/releases/latest/download/mobazha.exe
+  else
+    echo "Mobazha node already exists for Windows"
+  fi
+fi
+
+echo 'Installing npm packages...'
+npm install
+
+echo 'Installing frontend dependencies...'
+cd frontend
+npm install
+cd ..
 
 echo 'Building Mobazha app...'
-npm run build
+npm run clean
+npm run encrypt
+npm run build-frontend
 
-echo 'Copying transpiled files into js folder...'
-cp -rf prod/* js/
+echo "We are building for platform: ${1}"
 
-echo "We are building: ${BINARY}"
-
-case "$TRAVIS_OS_NAME" in
+case "$1" in
   "linux")
-
-    echo 'Linux builds'
-    echo 'Making dist directories'
-    mkdir dist/linux64
-
-    sudo apt-get install rpm
-
-    echo 'Install npm packages for Linux'
-    npm install -g --save-dev electron-installer-debian --silent
-    npm install -g --save-dev electron-installer-redhat@2.0.0 --silent
-
-    # Install libgconf2-4
-    sudo apt-get install libgconf2-4 libgconf-2-4
-
-    # Install rpmbuild
-    sudo apt-get --only-upgrade install rpm
-
-    # Ensure fakeroot is installed
-    sudo apt-get install fakeroot
-
-    # # Retrieve Latest Server Binaries
-    # sudo apt-get install jq
-    # cd MOBAZHA_TEMP/
-    # curl -u $GITHUB_USER:$GITHUB_TOKEN -s https://api.github.com/repos/OpenBazaar/openbazaar-go/releases/$SERVERTAG > release.txt
-    # cat release.txt | jq -r ".assets[].browser_download_url" | xargs -n 1 curl -L -O
-    # cd ..
-
-    APPNAME="mobazha"
-
-    echo 'Building Linux 64-bit Installer....'
-
-    echo "Packaging Electron application"
-    electron-packager . ${APPNAME} --platform=linux --arch=x64 --electronVersion=${ELECTRONVER} --overwrite --ignore="MOBAZHA_TEMP" --prune --out=dist
-
-    echo 'Move go server to electron app'
-    mkdir dist/${APPNAME}-linux-x64/resources/mobazha/
-    cp -rf MOBAZHA_TEMP/mobazha-linux-amd64 dist/${APPNAME}-linux-x64/resources/mobazha
-    rm -rf MOBAZHA_TEMP/*
-    mv dist/${APPNAME}-linux-x64/resources/mobazha/mobazha-linux-amd64 dist/${APPNAME}-linux-x64/resources/mobazha/mobazhad
-    rm -rf dist/${APPNAME}-linux-x64/resources/app/.travis
-    chmod +x dist/${APPNAME}-linux-x64/resources/mobazha/mobazhad
-
-    echo 'Create debian archive'
-    electron-installer-debian --config .travis/config_amd64.json
-
-    echo 'Create RPM archive'
-    electron-installer-redhat --config .travis/config_x86_64.json
-
-    APPNAME="mobazhaclient"
-
-    echo 'Building Linux 64-bit Installer....'
-
-    echo "Packaging Electron application"
-    electron-packager . ${APPNAME} --platform=linux --arch=x64 --ignore="MOBAZHA_TEMP" --electronVersion=${ELECTRONVER} --overwrite --prune --out=dist
-
-    echo 'Create debian archive'
-    electron-installer-debian --config .travis/config_amd64.client.json
-
-    echo 'Create RPM archive'
-    electron-installer-redhat --config .travis/config_x86_64.client.json
-
+    echo 'Building Linux Installer...'
+    npm run build-l
     ;;
-
   "osx")
-    if [[ $BINARY == 'win' ]]; then
-        echo 'Running Electron Packager...'
-        npm run package
-
-        echo 'Copying server binary into application folder...'
-        mkdir -p dist/Mobazha/resources/mobazha
-        cp -rf MOBAZHA_TEMP/mobazha-amd64.exe dist/Mobazha/resources/mobazha/mobazhad.exe
-        
-        npm run make
-    else
-        # OSX
-        echo 'Building OSX Installer'
-        mkdir dist/osx
-
-        # Sign mobazha binary
-        echo 'Signing Go binary'
-        cp MOBAZHA_TEMP/mobazha-darwin-amd64 dist/osx/mobazhad
-        # rm -rf MOBAZHA_TEMP/*
-        # codesign --force --sign "$SIGNING_IDENTITY2" --timestamp --options runtime dist/osx/mobazhad
-
-        echo 'Running Electron Packager...'
-        npm run package
-
-        echo 'Creating mobazha folder in the OS X .app'
-        mkdir out/Mobazha-darwin-x64/Mobazha.app/Contents/Resources/mobazha
-
-        echo 'Moving binary to correct folder'
-        cp MOBAZHA_TEMP/mobazha-darwin-amd64 out/Mobazha-darwin-x64/Mobazha.app/Contents/Resources/mobazha/mobazhad
-        chmod +x out/Mobazha-darwin-x64/Mobazha.app/Contents/Resources/mobazha/mobazhad
-        
-        npm run make
-    fi
-
-  ;;
+    echo 'Building macOS Installer...'
+    npm run build-m
+    ;;
+  "win")
+    echo 'Building Windows Installer...'
+    npm run build-w
+    ;;
+  *)
+    echo "Unsupported platform: ${1}"
+    exit 1
+    ;;
 esac
